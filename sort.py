3a20c9a4d2e2b68a71330f3ecd2ca5b2bbebb7d4
vodihvu <55524120+vodihvu@users.noreply.github.com>
2020-06-15 19:32:21 +0700

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,23 @@
+def bubble_sort(nums):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                # Swap the elements
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Set the flag to True so we'll loop again
+                swapped = True
+def selection_sort(A):
+    for i in range(len(A)):
+
+        # Find the minimum element in remaining
+        # unsorted array
+        min_idx = i
+        for j in range(i + 1, len(A)):
+            if A[min_idx] > A[j]:
+                min_idx = j
+
+                # Swap the found minimum element with
+        # the first element
+        A[i], A[min_idx] = A[min_idx], A[i]
---
+++ test_bubble_sort.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 15:28:24 2020
+
+@author: DINHVU
+"""
+
+
+from bubble_sort import bubble_sort
+random_list_of_nums=[5,2,1,4,8]
+bubble_sort(random_list_of_nums)
+print(random_list_of_nums)
